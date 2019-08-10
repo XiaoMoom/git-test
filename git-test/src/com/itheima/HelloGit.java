e5acdc4ba3132b111d3193edc3a653b3e7b21eb6
xiaomo <919870385@qq.com>
2019-08-10 08:38:19 +0800

    把本地工程添加到git

---
+++ git-test/src/com/itheima/HelloGit.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 黄佳豪先生
+ * Date: 2019-08-10
+ * Time: 8:26
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+
+}
